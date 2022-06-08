c735c1a381fe603ff19c35973d139ee822a456ff
Scifiler-sys <pagdilaostephen@gmail.com>
2022-06-08 13:47:08 -0400

    Added a new feature

--- SampleModel/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SampleModel;
-public class Class2
-{
-  public int StephenProperty { get; set; }
-  public int AntherProperty { get; set; }
-  public int ThridProperty { get; set; }
-  public int ChadelProperty { get; set; }
-}
